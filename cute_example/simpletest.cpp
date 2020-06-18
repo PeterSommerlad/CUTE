24c3939fd659ca3910807ef55a9214292356cea7
Dmitry Radzevich <dmitry.radzevich@truphone.com>
2020-06-18 18:16:04 +0200

    Added examples of indexed and checked throws tests

--- cute_example/simpletest.cpp
+++ cute_example/simpletest.cpp
@@ -36,7 +36,7 @@ void mySimpleTest(){
 #include <iostream>
 
 void main1(){
-	cute::ide_listener<> listener{};
+    cute::ide_listener<> listener{};
     if (cute::makeRunner(listener)(mySimpleTest)){
         std::cout << "success\n";
     } else {
@@ -53,34 +53,91 @@ void main2(){
 #include "cute_equals.h"
 
 int anotherTest(){
-	ASSERT_EQUAL(42, lifeTheUniverseAndEverything);
-	return 0;
+    ASSERT_EQUAL(42, lifeTheUniverseAndEverything);
+    return 0;
 }
 
 cute::test tests[]={
-	CUTE(mySimpleTest)
-	,mySimpleTest
-	,CUTE(anotherTest)
+    CUTE(mySimpleTest)
+    ,mySimpleTest
+    ,CUTE(anotherTest)
 };
 
 struct ATestFunctor {
-	void operator()(){
-		ASSERT_EQUAL_DELTA(42.0, static_cast<double>(lifeTheUniverseAndEverything), 0.001);
-	}
+    void operator()(){
+        ASSERT_EQUAL_DELTA(42.0, static_cast<double>(lifeTheUniverseAndEverything), 0.001);
+    }
 };
 
 #include "cute_suite.h"
 
 int main3(){
-	cute::ide_listener<> listener{};
+    cute::ide_listener<> listener{};
     auto run = cute::makeRunner(listener);
     cute::suite s(tests, tests + (sizeof(tests) / sizeof(tests[0])));
-	s += ATestFunctor();
-	return run(s, "suite");
+    s += ATestFunctor();
+    return run(s, "suite");
+    }
+
+#include "cute_indexed.h"
+
+struct Point3d {
+    int x, y, z;
+};
+
+Point3d makePoint(int i) {
+#ifdef FAIL_INDEXED_TEST
+    return Point3d{i, i + 1, i - 1};
+#else
+    return i == 5 ? Point3d{i, i, i} : Point3d{i, i + 1, i - 1};
+#endif 
+}
+
+struct Exception {
+    int code_; 
+    explicit Exception(int code) : code_(code) { }
+};
+
+void throwException(int code) {
+#ifdef FAIL_CHECKED_THROWS_TEST
+    throw Exception(code + 1);
+#else 
+    throw Exception(code);
+#endif
+}
+
+void testIndexed() {
+    for(int i = 0; i < 10; ++i) {
+        const auto refPoint = i == 5 ? Point3d{i, i, i} : Point3d{i, i + 1, i - 1};
+        const auto point = makePoint(i);
+        ASSERT_EQUALI(i, refPoint.x, point.x);
+        ASSERT_EQUALI(i, refPoint.y, point.y);
+        ASSERT_EQUALI(i, refPoint.z, point.z);
+    }
+}
+
+void testCheckedThrow() {
+    ASSERT_THROWS(throwException(0), Exception);
+    ASSERT_THROWS_CHECK(throwException(0), Exception, 
+        ASSERT_EQUAL(0, e.code_));
+}
+  
+
+int main4() {
+    static const cute::test tests[] = {
+        CUTE(testIndexed),
+        CUTE(testCheckedThrow),
+    };
+
+    cute::ide_listener<> listener{};
+    auto run = cute::makeRunner(listener);
+    cute::suite s(tests, tests + (sizeof(tests) / sizeof(tests[0])));
+    return run(s, "new test suite");
 }
 
 int main(){
-	main1();
-	main2();
-	main3();
+    main1();
+    main2();
+    main3();
+    main4();
 }
