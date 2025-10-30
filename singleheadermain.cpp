3ae0bdb244e3d439b332329f1dbb6438b7b7b6ce
PeterSommerlad <peter.cpp@sommerlad.ch>
2025-10-30 11:59:13 +0100

    provide single header main example

---
+++ singleheadermain.cpp
@@ -0,0 +1,19 @@
+#include "cute_sh.h"
+void thisIsATest() {
+	ASSERTM("start writing tests", false);
+}
+
+bool runAllTests(int argc, char const *argv[]) {
+	cute::suite s { };
+	//TODO add your test here
+	s.push_back(CUTE(thisIsATest));
+	cute::xml_file_opener xmlfile(argc, argv);
+	cute::xml_listener<cute::ide_listener<>> lis(xmlfile.out);
+	auto runner = cute::makeRunner(lis, argc, argv);
+	bool success = runner(s, "AllTests");
+	return success;
+}
+
+int main(int argc, char const *argv[]) {
+    return runAllTests(argc, argv) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
